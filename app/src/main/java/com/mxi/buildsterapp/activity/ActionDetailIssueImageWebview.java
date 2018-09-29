2915ad493efe6fb6247ddc15640d1fdd82bc1349
sensoft2207 <vishal.sensoft22@gmail.com>
2018-09-29 10:08:20 +0530

    initial

---
+++ app/src/main/java/com/mxi/buildsterapp/activity/ActionDetailIssueImageWebview.java
@@ -0,0 +1,179 @@
+package com.mxi.buildsterapp.activity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import com.mxi.buildsterapp.R;
+import com.mxi.buildsterapp.comman.CheckNetworkConnection;
+import com.mxi.buildsterapp.comman.CommanClass;
+
+public class ActionDetailIssueImageWebview extends AppCompatActivity {
+
+    boolean clearHistory = false;
+
+    CommanClass cc;
+
+    WebView home_web;
+
+    ProgressBar pb;
+    ImageView iv_no_network,iv_close;
+
+
+    String url,def_id;
+
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message message) {
+            switch (message.what) {
+                case 1:{
+                    webViewGoBack();
+                }break;
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webview);
+
+        cc = new CommanClass(this);
+
+        home_web = (WebView)findViewById(R.id.home_web);
+        pb = (ProgressBar) findViewById(R.id.pb);
+        iv_no_network = (ImageView) findViewById(R.id.iv_no_network);
+        iv_close = (ImageView) findViewById(R.id.iv_close);
+
+        def_id = cc.loadPrefString("action_def_id");
+
+        url = getIntent().getStringExtra("issue_url");
+
+        startWebView(url);
+
+        Log.e("URLLL",url);
+
+        iv_close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                onBackPressed();
+                overridePendingTransition(R.anim.slide_from_left, R.anim.slide_to_right);
+            }
+        });
+    }
+
+    private void startWebView(String url) {
+
+        if(CheckNetworkConnection.isInternetAvailable(this))
+        {
+
+
+            home_web.getSettings().setJavaScriptEnabled(true);
+
+            home_web.getSettings().setAppCacheEnabled(true);
+            // settings.setBuiltInZoomControls(true);
+            home_web.getSettings().setPluginState(WebSettings.PluginState.ON);
+            home_web.getSettings().setJavaScriptEnabled(true);
+            home_web.getSettings().setBuiltInZoomControls(false);
+            home_web.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+            home_web.getSettings().setDatabasePath("/data/data/" + home_web.getContext().getPackageName() + "/databases/");
+            home_web.getSettings().setDomStorageEnabled(true);
+
+            home_web.setWebChromeClient(new WebChromeClient() {
+                public void onProgressChanged(WebView view, int progress) {
+                    pb.setProgress(progress);
+                    if (progress == 100) {
+                        pb.setVisibility(View.GONE);
+                        //iv_no_network.setVisibility(View.GONE);
+
+                    } else {
+                        pb.setVisibility(View.VISIBLE);
+                        //iv_no_network.setVisibility(View.GONE);
+                    }
+                }
+            });
+            home_web.setWebViewClient(new WebViewClient() {
+                public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+
+                    home_web.loadData("<center><b></b></center>", "text/html", null);
+                    iv_no_network.setVisibility(View.VISIBLE);
+                }
+
+                @Override
+                public boolean shouldOverrideUrlLoading(WebView view, String url) {
+
+                    view.loadUrl(url);
+                    //view.loadUrl(url);
+
+                    return false;
+                }
+            });
+
+            home_web.setOnKeyListener(new View.OnKeyListener(){
+
+                @Override
+                public boolean onKey(View v, int keyCode, KeyEvent event) {
+                    if (keyCode == KeyEvent.KEYCODE_BACK
+                            && event.getAction() == MotionEvent.ACTION_UP
+                            && home_web.canGoBack()) {
+                        handler.sendEmptyMessage(1);
+                        return true;
+                    }
+
+                    return false;
+                }
+
+            });
+
+
+
+            home_web.loadUrl(url);
+
+
+        }
+        else{
+
+            iv_no_network.setVisibility(View.VISIBLE);
+            pb.setVisibility(View.GONE);
+        }
+
+    }
+
+    private void webViewGoBack() {
+
+        home_web.goBack();
+
+        home_web.clearHistory();
+
+        finish();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        home_web.clearHistory();
+        home_web.clearCache(true);
+    }
+
+   /* @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+
+        finish();
+
+    }*/
+}
---
+++ app/src/main/java/com/mxi/buildsterapp/activity/ActionDetailIssueImgSlider.java
@@ -0,0 +1,367 @@
+package com.mxi.buildsterapp.activity;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
+import com.mxi.buildsterapp.R;
+import com.mxi.buildsterapp.adapter.ViewPagerAdapter;
+import com.mxi.buildsterapp.comman.AppController;
+import com.mxi.buildsterapp.comman.CommanClass;
+import com.mxi.buildsterapp.comman.Const;
+import com.mxi.buildsterapp.model.KidTaskData;
+import com.mxi.buildsterapp.utils.ExtendedViewPager;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ActionDetailIssueImgSlider extends AppCompatActivity {
+
+    CommanClass cc;
+
+    ExtendedViewPager vp_slider;
+    private ArrayList<KidTaskData> images;
+    private FragmentStatePagerAdapter adapter;
+    ArrayList<KidTaskData> kid_task_list;
+
+    ImageView iv_close,iv_previous,iv_next;
+
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.actiondetail_issue_image_slider);
+
+        init();
+    }
+
+    private void init() {
+
+        cc = new CommanClass(this);
+        iv_previous = (ImageView) findViewById(R.id.iv_previous);
+        iv_next = (ImageView) findViewById(R.id.iv_next);
+        vp_slider = (ExtendedViewPager) findViewById(R.id.vp_slider);
+        iv_close = (ImageView) findViewById(R.id.iv_close);
+
+        if (!cc.isConnectingToInternet()) {
+
+            cc.showToast(getString(R.string.no_internet));
+
+        } else {
+
+            if(getIntent().getStringExtra("from").equals("myproject")){
+
+                getDeficiencyDetailWS();
+
+            }else {
+
+                getDeficiencyDetailWSTwo();
+            }
+        }
+
+        iv_previous.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (vp_slider.getCurrentItem() > 0) {
+                    vp_slider.setCurrentItem(vp_slider.getCurrentItem() - 1);
+                }
+
+            }
+        });
+
+        iv_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (vp_slider.getCurrentItem() < vp_slider.getAdapter().getCount() - 1) {
+                    vp_slider.setCurrentItem(vp_slider.getCurrentItem() + 1);
+                }
+
+            }
+        });
+
+
+
+        iv_close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                onBackPressed();
+                overridePendingTransition(R.anim.slide_from_left, R.anim.slide_to_right);
+            }
+        });
+    }
+
+
+    private void getDeficiencyDetailWS() {
+
+        final ProgressDialog pDialog = new ProgressDialog(this);
+        pDialog.setMessage("Please wait...");
+        pDialog.setCancelable(false);
+        pDialog.show();
+
+        StringRequest jsonObjReq = new StringRequest(Request.Method.POST, Const.ServiceType.GET_DEFICIENCY_INFORMATION,
+                new Response.Listener<String>() {
+
+
+                    @Override
+                    public void onResponse(String response) {
+                        Log.e("response:def data", response);
+
+                        images = new ArrayList<>();
+
+                        kid_task_list = new ArrayList<>();
+
+
+                        try {
+
+                            JSONObject jsonObject = new JSONObject(response);
+
+                            if (jsonObject.getString("status").equals("200")) {
+
+                                pDialog.dismiss();
+
+                                JSONArray dataArray = jsonObject.getJSONArray("deficiency_images");
+
+                                for (int i = 0; i < dataArray.length(); i++) {
+
+                                    JSONObject jsonObject1 = dataArray.getJSONObject(i);
+
+
+                                    KidTaskData k_model = new KidTaskData();
+                                    k_model.setTaskImage(jsonObject1.getString("def_image"));
+
+                                    kid_task_list.add(k_model);
+                                }
+
+                                JSONArray dataArray2 = jsonObject.getJSONArray("deficiency_information");
+
+                                for (int i = 0; i < dataArray2.length(); i++) {
+
+                                    JSONObject jsonObject2 = dataArray2.getJSONObject(i);
+
+                                    String screen_image = jsonObject2.getString("screen_image");
+                                    String status = jsonObject2.getString("status");
+
+                                    String x_cor = jsonObject2.getString("posX");
+                                    String y_cor = jsonObject2.getString("posY");
+
+                                    float x = Float.parseFloat(x_cor);
+                                    float y = Float.parseFloat(y_cor);
+
+                                }
+
+                                setImagesData();
+
+                                adapter = new ViewPagerAdapter(getSupportFragmentManager(), images);
+                                vp_slider.setAdapter(adapter);
+
+
+
+                            } else if (jsonObject.getString("status").equals("404")) {
+
+                                pDialog.dismiss();
+
+
+                            } else {
+                                pDialog.dismiss();
+                                cc.showToast(jsonObject.getString("message"));
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                pDialog.dismiss();
+                cc.showToast(getString(R.string.ws_error));
+            }
+        }) {
+
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("user_id", cc.loadPrefString("user_id"));
+                params.put("project_id", cc.loadPrefString("project_id_main"));
+                params.put("screen_id", cc.loadPrefString("action_screen_id"));
+                params.put("deficiency_id", cc.loadPrefString("action_def_id"));
+
+                Log.e("Request def", String.valueOf(params));
+
+                return params;
+            }
+
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<>();
+                headers.put("Authorization", Const.Authorizations.AUTHORIZATION);
+                headers.put("UserAuth", cc.loadPrefString("user_token"));
+                Log.i("request header", headers.toString());
+                return headers;
+            }
+
+        };
+
+        AppController.getInstance().addToRequestQueue(jsonObjReq, "Temp");
+
+
+    }
+
+    private void getDeficiencyDetailWSTwo() {
+
+        final ProgressDialog pDialog = new ProgressDialog(this);
+        pDialog.setMessage("Please wait...");
+        pDialog.setCancelable(false);
+        pDialog.show();
+
+        StringRequest jsonObjReq = new StringRequest(Request.Method.POST, Const.ServiceType.GET_DEFICIENCY_INFORMATION,
+                new Response.Listener<String>() {
+
+
+                    @Override
+                    public void onResponse(String response) {
+                        Log.e("response:def data", response);
+
+                        images = new ArrayList<>();
+
+                        kid_task_list = new ArrayList<>();
+
+
+                        try {
+
+                            JSONObject jsonObject = new JSONObject(response);
+
+                            if (jsonObject.getString("status").equals("200")) {
+
+                                pDialog.dismiss();
+
+                                JSONArray dataArray = jsonObject.getJSONArray("deficiency_images");
+
+                                for (int i = 0; i < dataArray.length(); i++) {
+
+                                    JSONObject jsonObject1 = dataArray.getJSONObject(i);
+
+
+                                    KidTaskData k_model = new KidTaskData();
+                                    k_model.setTaskImage(jsonObject1.getString("def_image"));
+
+                                    kid_task_list.add(k_model);
+                                }
+
+                                JSONArray dataArray2 = jsonObject.getJSONArray("deficiency_information");
+
+                                for (int i = 0; i < dataArray2.length(); i++) {
+
+                                    JSONObject jsonObject2 = dataArray2.getJSONObject(i);
+
+                                    String screen_image = jsonObject2.getString("screen_image");
+                                    String status = jsonObject2.getString("status");
+
+                                    String x_cor = jsonObject2.getString("posX");
+                                    String y_cor = jsonObject2.getString("posY");
+
+                                    float x = Float.parseFloat(x_cor);
+                                    float y = Float.parseFloat(y_cor);
+
+                                }
+
+                                setImagesData();
+
+                                adapter = new ViewPagerAdapter(getSupportFragmentManager(), images);
+                                vp_slider.setAdapter(adapter);
+
+
+
+                            } else if (jsonObject.getString("status").equals("404")) {
+
+                                pDialog.dismiss();
+
+
+                            } else {
+                                pDialog.dismiss();
+                                cc.showToast(jsonObject.getString("message"));
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                pDialog.dismiss();
+                cc.showToast(getString(R.string.ws_error));
+            }
+        }) {
+
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("user_id", cc.loadPrefString("user_id"));
+                params.put("project_id", cc.loadPrefString("project_id_assigned"));
+                params.put("screen_id", cc.loadPrefString("action_a_screen_id"));
+                params.put("deficiency_id", cc.loadPrefString("action_a_def_id"));
+
+                Log.e("Request def", String.valueOf(params));
+
+                return params;
+            }
+
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<>();
+                headers.put("Authorization", Const.Authorizations.AUTHORIZATION);
+                headers.put("UserAuth", cc.loadPrefString("user_token"));
+                Log.i("request header", headers.toString());
+                return headers;
+            }
+
+        };
+
+        AppController.getInstance().addToRequestQueue(jsonObjReq, "Temp");
+
+
+    }
+
+    private void setImagesData() {
+
+        for (int i = 0; i < kid_task_list.size(); i++) {
+
+            KidTaskData kd = kid_task_list.get(i);
+
+            kd.getTaskImage();
+            kd.getTask_id();
+
+            images.add(kd);
+        }
+    }
+
+}
